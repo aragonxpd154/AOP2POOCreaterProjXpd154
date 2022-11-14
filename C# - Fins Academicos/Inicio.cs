63a1c21c9ceaa3cf207ecf08f11675f82e4af502
Marcos Obel <marcos.silvabl@live.com>
2022-11-13 22:02:28 -0300

    update

---
+++ C# - Fins Academicos/Inicio.cs
@@ -0,0 +1,30 @@
+﻿
+// AOP2 POO 
+/*
+__version__ = 0.1
+__date__ = "13-09-2022"
+__authors__ = 
+{
+Amanda Leandro Quinteiro Lopes
+Marcos Silva dos Santos
+Luiz Michel Franco
+Camilly Lourenço de Assis
+}
+
+__license__ = "License GPL v3.0"
+*/
+
+namespace AOPPOO
+{
+    internal class Inicio
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando classes
+            Funcionario linkClassFuncionario = new();
+
+            // Executando o metodo da classe Loja.Menu();
+            linkClassFuncionario.ValidarFuncionario();
+        }
+    }
+}
