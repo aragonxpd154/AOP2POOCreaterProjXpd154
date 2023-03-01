ffee5b573ca8ffc98190565acb1e0bc15c57d1eb
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-01 22:00:45 +0000

    mutavel_string_teste

---
+++ py/Unit 2/object_multavel_inmult.py
@@ -0,0 +1,21 @@
+# Verificação de ID de memoria de cada variavel atravez do metodo ID
+
+a = 95
+print(f'A variavel a com valor {a} esta no endereço {id(a)}')
+a = a + 1
+print(f'A variavel a com valor {a} esta no endereço {id(a)}')
+
+lista = ['a', 'b']
+print(f'A variavel a com valor {lista} esta no endereço {id(lista)}')
+
+lista.append('c')
+print(f'A variavel a com valor {lista} esta no endereço {id(lista)}')
+
+def naoAlteraString(palavra):
+    print(f'A palavra inicial (dentro da função) é {palavra}')
+    palavra = 'nova'
+    print(f'A palavra final (dentro da função) é: {palavra}')
+
+minhaPalavra = 'antiga'
+naoAlteraString(minhaPalavra)
+print(f'A palavra é: {minhaPalavra}')
