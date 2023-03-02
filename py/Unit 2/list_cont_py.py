4a3130b300c516083cfb0cb4418bcc96b8b1c1e6
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-02 10:35:44 +0000

    Utilizando função len()

--- py/Unit 2/list_cont_py.py
+++ py/Unit 2/list_cont_py.py
@@ -16,4 +16,12 @@ print (f'O valor da lista [e {listaSimples}]')
 listaVariosTipos = ['a', 1, 'python', 15.2]
 print(f'O valor da listaVariosTipo é {listaVariosTipos}')
 
+# Criando Lista dentro de lista
+listaCompleta = [listaSimples, listaVariosTipos]
+print(f'O valor da listaCompleta é  {listaCompleta}')
 
+print(f'O valor da listaSimples na posição nº 1 é {listaSimples[1]}')
+
+# Descobrindo tamanho da lista com a função len()
+
+print(f'O ultimo indice de listaSimple é {len(listaSimples)-1}')
