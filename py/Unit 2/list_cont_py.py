5921e6fc1b6cc667b73482a08545d06eedc1b602
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-02 10:29:42 +0000

    List test

--- py/Unit 2/list_cont_py.py
+++ py/Unit 2/list_cont_py.py
@@ -2,5 +2,18 @@
 primeiraLista = ['a']
 print(f'O contéudo de primeiraLista é {primeiraLista}')
 
-
 # Criando Lista vazias
+
+# Declaração de lista Formal
+L = list('a')
+print(L)
+
+# Criando Lista de tipo inteiro
+listaSimples  = [1, 2, 3, 4]
+print (f'O valor da lista [e {listaSimples}]')
+
+# Criando lista de varios tipos de dados
+listaVariosTipos = ['a', 1, 'python', 15.2]
+print(f'O valor da listaVariosTipo é {listaVariosTipos}')
+
+
