234c6bb4fadf29ce42e0b4f08c1e555d2029772f
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-03 15:55:39 +0000

    Trabalhando com lista encadeadas

---
+++ py/Unit 4/list_encadernade.py
@@ -0,0 +1,6 @@
+class Nodo:def __init__(self, dado=0, proximoNodo=None):
+    self.dado = dado
+    self.proximo = proximoNodo
+    
+    def __repr__(self):
+        return f"{self.dado}" -> {self.dado}
