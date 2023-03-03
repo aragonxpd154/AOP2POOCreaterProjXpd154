eaa739703080b9185c630a3c2d859b6c326c00ec
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-03 15:56:50 +0000

    Adequando

--- py/Unit 4/list_encadernade.py
+++ py/Unit 4/list_encadernade.py
@@ -1,6 +1,8 @@
-class Nodo:def __init__(self, dado=0, proximoNodo=None):
-    self.dado = dado
-    self.proximo = proximoNodo
+class Nodo:
+    def __init__(self, dado=0, proximoNodo=None):
+        self.dado = dado
+        self.proximo = proximoNodo
     
     def __repr__(self):
-        return f"{self.dado}" -> {self.dado}
+        return f"{self.dado} -> {self.dado}"
+    
