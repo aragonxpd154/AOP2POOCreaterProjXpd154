7d18c1de3686afc62a655f9690e85f5b778c2653
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-03 16:07:23 +0000

    Adequação de lista simples encadeada

--- py/Unit 4/list_encadernade.py
+++ py/Unit 4/list_encadernade.py
@@ -1,3 +1,10 @@
+# A segue uma classe para representação do nodo
+
+# Nele esta armazenado duas informações importante, valor e referência para o próximo elemento
+# O método __repr__ define o comportamento do Nodo quando ele é impresso com a função print()
+# E o ultimo método o setProximo, é responsamento por atuializar a referencia para o proximo nodo quando
+# um novo for inserido.
+
 class Nodo:
     def __init__(self, dado=0, proximoNodo=None):
         self.dado = dado
@@ -9,3 +16,9 @@ class Nodo:
     def setProximo(self, proximoNodo):
         self.proximo = proximoNodo
     
+
+# A segui, criando um nodo com instância do objeto
+
+nodo = Nodo(5)
+print(f"O valor do dado é {nodo.dado} e a \ referência para o proximo elemento é {nodo.proximo}")
+print(f'A representação do Nodo é; {nodo}')
