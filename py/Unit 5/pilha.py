f638241bd8db119193f855e70b970243ae834a7f
Marcos (Obel) <marcos.silvabl@live.com>
2023-03-12 15:18:22 +0000

    pilha update

--- py/Unit 5/pilha.py
+++ py/Unit 5/pilha.py
@@ -13,3 +13,22 @@ print ("Pilha: ", pilha)
 pilha.append(8)
 print("Inserindo um elemento: ", pilha)
 
+pilha.append(13)
+print("Inserindo outro elemento:", pilha)
+
+pilha.pop()
+print("REmovendo um elemento: ", pilha)
+
+pilha.pop()
+print("Removendo outro elemento ", pilha)
+
+print ("\n\n\n\n\n\n")
+
+class Node:
+    def __init__(self, dado):
+        self.dado = dado
+        self.anterior = None
+        
+    def __repr__(self):
+        return f'{self.dado} -> {self.anterior}
+    '
